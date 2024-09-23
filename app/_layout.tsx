64d0ec0d66cf72e73f604409ead11792e7543e06
Andrés Martinez-Losa <asvimalo@gmail.com>
2024-09-23 21:08:51 +0200

    det

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,6 @@ import { View } from "react-native";
 
 export default function RootLayout() {
   return (
-    
     <Stack>
         <Stack.Screen name="index" options={{ title: "Counter" }} />
     </Stack>
