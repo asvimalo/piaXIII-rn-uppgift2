a0292731b63887372dc1193a0ba2b04b686bbc5e
Andrés Martinez-Losa <asvimalo@gmail.com>
2024-09-23 15:50:42 +0200

    plus minus not minor than zero implemented

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,51 @@
 import { useState } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Button } from "react-native";
 
 export default function Index() {
 
   const [count, setCount] = useState(0)
+
+//PLUS" och "MINUS" som ökad/minskar siffran med 1. Låt inte siffra bli mindre än noll.
+  function onPlus() {
+    setCount(count + 1)
+  }
+  function onMinus() {
+    if (count > 0) {
+      setCount(count - 1)
+    }
+    
+  }
   return (
-    <View
-      style={styles.vista}
-    >
-      <Text style={styles.numero}> {count} </Text>
-    </View>
+    <>
+      <View style={styles.vista1}>
+        <Text style={styles.numero}> {count} </Text>
+      </View>
+      <View style={styles.vista2}>
+      <Button
+        onPress={onPlus}
+        title="PLUS"
+        color="green"
+        
+      />
+      <Button
+        onPress={onMinus}
+        title="MINUS"
+        color="red"
+        
+      />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  vista: {
+  vista1: {
     flex: 1,
     //justifyContent: "center",
     alignItems: "center",
+  },
+  vista2: {
+    flex: 5,
   },
   numero: {
     fontSize: 30
