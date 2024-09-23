2862539fb05d75e1294e401c01ded91fc09634c6
Andrés Martinez-Losa <asvimalo@gmail.com>
2024-09-23 20:48:05 +0200

    tittle to navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,11 @@
 import { Stack } from "expo-router";
+import { View } from "react-native";
 
 export default function RootLayout() {
   return (
+    
     <Stack>
-      <Stack.Screen name="index"  />
+        <Stack.Screen name="index" options={{ title: "Counter" }} />
     </Stack>
   );
 }
