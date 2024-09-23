bb6f4dabd1d6cf343b76c9ee411016d4d7d98209
Andrés Martinez-Losa <asvimalo@gmail.com>
2024-09-23 15:30:28 +0200

    index changed with number centered upp

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,25 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
+
+  const [count, setCount] = useState(0)
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.vista}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={styles.numero}> {count} </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  vista: {
+    flex: 1,
+    //justifyContent: "center",
+    alignItems: "center",
+  },
+  numero: {
+    fontSize: 30
+  }
+})
